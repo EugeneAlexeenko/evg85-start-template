ba7579e8e5712929d9c642798fde63385efad519
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2017-05-12 15:57:20 +0300

    sass task done

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,38 @@ var gulp         = require('gulp'),
     del          = require('del'),
     sourcemaps   = require('gulp-sourcemaps'),
     sass         = require('gulp-sass'),
-    autoprefixer = require('autoprefixer'),
+    concat       = require('gulp-concat'),
     browsersync  = require('browser-sync');
 
 // настройка путей
 var path = {
-  build: {},
+  build: {
+    js: 'build/js/'
+  },
   src:   {
-    sass: 'src/styles/main.scss',
-    css:  'src/styles'
+    sass:    'src/styles/main.scss',
+    css:     'src/styles/',
+    libs:    ['src/libs/jquery-3.2.1.min.js',
+              //'src/libs/jquery.magnific-popup.js'
+            ]
   },
   watch: {},
   clean: ['build', 'src/styles/main.css']
 };
 
-// Очистка папок и файлов
+// Очистка папок и файлов, указанных в path.clean
 gulp.task('clean', function() {
   return del(path.clean);
+});
+
+
+// Сборка всех стилей в main.css
+gulp.task("sass", function () {
+  return gulp.src(path.src.sass)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.src.css));
 });
 
 
@@ -31,18 +46,4 @@ gulp.task('browser-sync', function() {
     },
     notify: false
   });
-});
-
-// Сборка стилей
-gulp.task("sass", function () {
-  return gulp.src(path.src.sass)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    //.pipe(autoprefixer(['last 2 versions']))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.src.css));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
 });
