ac9a2d08e1796dd14eccbff393971e8a35f6f9a0
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2017-05-12 16:45:39 +0300

    add libs task

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,18 @@ var path = {
     js: 'build/js/'
   },
   src:   {
-    sass:    'src/styles/main.scss',
-    css:     'src/styles/',
-    libs:    ['src/libs/jquery-3.2.1.min.js',
-              //'src/libs/jquery.magnific-popup.js'
-            ]
+    sass:   'src/styles/main.scss',
+    css:    'src/styles/',
+    js:     'src/js/',
+    libs:  ['src/libs/jquery-3.2.1.min.js',
+            'src/libs/Magnific-Popup/jquery.magnific-popup.min.js',
+           ]
   },
   watch: {},
-  clean: ['build', 'src/styles/main.css']
+  clean: ['build',
+          'src/styles/main.css',
+          'src/js/libs.js',
+         ]
 };
 
 // Очистка папок и файлов, указанных в path.clean
@@ -30,12 +34,19 @@ gulp.task('clean', function() {
 
 
 // Сборка всех стилей в main.css
-gulp.task("sass", function () {
+gulp.task('sass', function () {
   return gulp.src(path.src.sass)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(path.src.css));
+});
+
+// Сборка libs
+gulp.task('libs', function() {
+  return gulp.src(path.src.libs)
+    .pipe(concat('libs.js'))
+    .pipe(gulp.dest(path.src.js))
 });
 
 
